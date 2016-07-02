ea73db0cc50ae889fc19e2993c1272773ac314df
Johannes Flood <johannes_flood@hotmail.com>
2016-07-02 18:23:57 +0200

    Initial init

---
+++ MyWorld/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyWorld
+//
+//  Created by Johannes Flood on 2016-07-02.
+//  Copyright © 2016 Johannes Flood. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MyWorld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyWorld
+//
+//  Created by Johannes Flood on 2016-07-02.
+//  Copyright © 2016 Johannes Flood. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
